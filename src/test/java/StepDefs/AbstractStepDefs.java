519cb78cba1d9b991bfce524ff07a6fff1125110
Ankita <y.ankita.rao@gmail.com>
2022-12-07 12:19:17 +0100

    Updating AbstractStepDefs.java

--- src/test/java/StepDefs/AbstractStepDefs.java
+++ src/test/java/StepDefs/AbstractStepDefs.java
@@ -15,6 +15,21 @@ public class AbstractStepDefs {
     protected static HomePage homePage;
 
     static {
+
+        /*
+        System.out.println("Inside Step - browser is open");
+
+        String projectPath = System.getProperty("user.dir");
+        System.out.println("Project path is:" + projectPath);
+
+        System.setProperty("webdriver.chrome.driver", projectPath+"/src/test/resources/drivers/chromedriver.exe");
+
+        Driver = new ChromeDriver();
+
+        Driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        Driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+         */
+
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(WAIT_TIME, TimeUnit.SECONDS);
