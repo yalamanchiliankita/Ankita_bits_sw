70b759b4f091fab82ae4a1bd8ec333f415d6669a
Ankita <y.ankita.rao@gmail.com>
2022-12-07 12:14:08 +0100

    Adding InvalidLoginStepDefs.java

---
+++ src/test/java/StepDefs/InvalidLoginStepDefs.java
@@ -0,0 +1,26 @@
+package StepDefs;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class InvalidLoginStepDefs extends AbstractStepDefs {
+
+    WebDriver Driver = homePage.driver;
+
+    @When("user enters username and password")
+    public void user_enters_username_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        homePage.inputEmail.sendKeys("Ankita@gmail.com");
+        homePage.inputPassword.sendKeys("12345");
+    }
+
+    @Then("user is navigated to the same page")
+    public void user_is_navigated_to_the_same_page() {
+        // Write code here that turns the phrase above into concrete actions
+        homePage.loginButton.isDisplayed();
+        //Driver.close();
+       // Driver.quit();
+    }
+
+}
