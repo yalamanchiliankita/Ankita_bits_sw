1bd0ca38284b0d15c70558c3c5eb34b38d8d6aed
Ankita <y.ankita.rao@gmail.com>
2022-12-07 18:08:29 +0100

    Removing errors for Logout feature

--- src/test/java/StepDefs/LogoutStepDefs.java
+++ src/test/java/StepDefs/LogoutStepDefs.java
@@ -3,10 +3,20 @@ package StepDefs;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LogoutStepDefs extends AbstractStepDefs{
 
     WebDriver Driver = homePage.driver;
+
+    @And("user enters credentials with clicking login")
+    public void credentials_entered()
+    {
+        System.out.println("Automation test bot enters hardcoded email and password");
+        Driver.findElement(By.id("inputEmail")).sendKeys("y.ankita.rao@gmail.com");
+        Driver.findElement(By.id("inputPassword")).sendKeys("123456");
+        homePage.clickLogin();
+    }
 
     @And("user is on client page")
     public void user_on_client_page()
@@ -18,7 +28,13 @@ public class LogoutStepDefs extends AbstractStepDefs{
     public void click_logout()
     {
         System.out.println("Logging out from the client page");
-        Driver.findElement(By.id("Secondary_Sidebar-Client_Shortcuts-Logout")).click();
+        // Driver.findElement(By.id("Secondary_Navbar-Account-Logout")).click();
+        WebElement logoutButton = Driver.findElement(By.xpath("//a[@class = 'dropdown-item px-2 py-0']"));
+        System.out.println(logoutButton);
+        if(logoutButton != null)
+        {
+            System.out.println("Found logout button");
+        }
     }
 
     @Then("user is navigated back to the home page")
--- src/test/resources/Features/Logout.feature
+++ src/test/resources/Features/Logout.feature
@@ -1,8 +1,10 @@
 Feature: Checking logout Functionality
   Background:
     Given browser is open
+    And user is on login page
 
   Scenario: Check logout functionality works properly
+    And user enters credentials with clicking login
     And user is on client page
     When user clicks on logout
     Then user is navigated back to the home page
