c028ef87cbf3e5b4fa6923f4137e0c80cc37cb0b
Ankita <y.ankita.rao@gmail.com>
2022-12-07 12:16:46 +0100

    Adding LogoutStepDefs.java

---
+++ src/test/java/StepDefs/LogoutStepDefs.java
@@ -0,0 +1,31 @@
+package StepDefs;
+
+import io.cucumber.java.en.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LogoutStepDefs extends AbstractStepDefs{
+
+    WebDriver Driver = homePage.driver;
+
+    @And("user is on client page")
+    public void user_on_client_page()
+    {
+        homePage.clientPage();
+    }
+
+    @When("user clicks on logout")
+    public void click_logout()
+    {
+        Driver.findElement(By.id("Secondary_Sidebar-Client_Shortcuts-Logout")).click();
+    }
+
+    @Then("user is navigated back to the home page")
+    public void homePage()
+    {
+        homePage.openPage();
+        homePage.loginButton.isDisplayed();
+        //Driver.close();
+        //Driver.quit();
+    }
+}
